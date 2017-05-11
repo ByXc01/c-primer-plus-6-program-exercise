684997c1443feb5c23d8bc5c78474daaba5e1f23
ByXc <byxc@vip.qq.com>
2017-05-11 13:02:15 +0800

    2.12.1

---
+++ program_exercise2.12.1/my_name.c
@@ -0,0 +1,13 @@
+/* 20170511 -- ByXc */
+/* compilation compile: msys2 gcc6.3.0 */
+
+#include <stdio.h>
+int main(void)
+{
+    printf("ByXc \n");
+    printf("By \nXc \n");
+    printf("By");
+    printf("Xc");
+
+    return 0;
+}
